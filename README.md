eb7aaf9b0d3daea9108d7748788adcb24c068ff7
ktunebe <ktunebe@gmail.com>
2024-03-11 12:50:20 -0500

    Finished working initial app

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# week3-mini-project-rps
+# rps-mini-project
+
+## Description
+
+This is a project for UW bootcamp in which a game of rock, paper, scissors is simulated against an opponent.
+
+## Installation
+
+N/A
+
+## Usage
+
+This page was built to be responsive on multiple screen sizes. By clicking anywhere on any of the code snippets in the cards, the entire snippet will be selected for easy copying.
+
+Link to deployed application: 
+
+![Screenshot of the RPS Game Page]()
+
+## Credits
+
+Emojis for rock, paper and scissors were pasted from https://emojipedia.org/.
+
+## License
+
+N/A
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,41 +9,130 @@ let userR = 0;
 let userP = 0;
 let userS = 0;
 // Set emojis for RPS
-let rock = "🪨";
-let paper = "📄";
-let scissors = "✂️";
+const rock = "🪨";
+const paper = "📄";
+const scissors = "✂️";
+// Set button click
+const startButton = document.querySelector("button");
+
+// Function to play a game
+const playGame = function() {
+      userChoice = prompt('Type "R" for Rock, "P" for Paper, or "S" for Scissors');
+      if (userChoice === "R" || userChoice === "P" || userChoice === "S" || userChoice === "r" || userChoice === "p" || userChoice === "s") {
+            userChoice = userChoice.toUpperCase();
+            autoChoice();
+            showHeadings();
+            displayUserChoice();
+            displayOppChoice();
+            setTimeout(gameResult, 100);
+      } else if (userChoice === null) {
+            endGame();
+      } else {
+            playGame()
+      };
+}
+// Function to remove RPS display
+const removeDisplay = function() {
+      document.getElementById("opp-guess-heading").textContent = "";
+      document.getElementById("user-guess-heading").textContent = "";
+      document.getElementById("opp-guess-result").textContent = "";
+      document.getElementById("user-guess-result").textContent = "";
+}
+// Functions for display win/loss/tie and choices
+const displayRecord = function() {
+      document.getElementById("wins").textContent = `Wins: ${wins}`;
+      document.getElementById("losses").textContent = `Losses: ${losses}`;
+      document.getElementById("ties").textContent = `Ties: ${ties}`;
+}
+const displayChoiceHistory = function() {
+      document.getElementById("rocks").textContent = `You chose Rock ${userR} times.`;
+      document.getElementById("papers").textContent = `You chose Paper ${userP} times.`;
+      document.getElementById("scissors").textContent = `You chose Scissors ${userS} times.`;
+}
+
+// Function to end the game
+const endGame = function() {
+      let quit = confirm("Do you really want to quit?");
+      if (quit === false) {
+            playGame();
+      } else if (quit === true) {
+            removeDisplay();
+            displayRecord();
+            displayChoiceHistory();
+      }
+}
+// Function for play again?
+const playAgain = function() {
+      let yesPlay = confirm("Play again?");
+      if (yesPlay === true) {
+            playGame();
+      } else if (yesPlay === false) {
+            endGame();
+      }
+}
+
+// Function for win/loss/tie result
+const gameResult = function() {
+      if ((userChoice === "R" && oppChoice === "S") || (userChoice === "S" && oppChoice === "P") || (userChoice === "P" && oppChoice === "R")) {
+            alert("You win!");
+            wins++;
+            playAgain();
+      } else if ((userChoice === "R" && oppChoice === "P") || (userChoice === "S" && oppChoice === "R") || (userChoice === "P" && oppChoice === "S")) {
+            alert("You lose!");
+            losses++;
+            playAgain();
+      } else if (userChoice === oppChoice) {
+            alert("Tie!");
+            ties++;
+            playAgain();
+      }
+}
 
 // Functions for displaying results after guess
 showChoice = function(id, choice) {
       document.getElementById(id).textContent = choice;
 }
 showHeadings = function() {
-      document.getElementById("your-guess-heading").textContent = "Your Guess";
+      document.getElementById("user-guess-heading").textContent = "Your Guess";
       document.getElementById("opp-guess-heading").textContent = "Opponent's Guess";
 }
 
 // Generate opponent choice
-const newChoice = function() {
-      autoChoice = choices[Math.floor(Math.random() * 3)];
+const autoChoice = function() {
+      oppChoice = choices[Math.floor(Math.random() * 3)];
       return;
 };
 // Display opponent choice
-const displayChoice = function() {
-      if (autoChoice === "R") {
+const displayOppChoice = function() {
+      if (oppChoice === "R") {
             showChoice("opp-guess-result", rock);
-      } else if (autoChoice === "P") {
+      } else if (oppChoice === "P") {
             showChoice("opp-guess-result", paper);
-      } else if (autoChoice === "S") {
+      } else if (oppChoice === "S") {
             showChoice("opp-guess-result", scissors);
       }
 };
+// Display user choice
+const displayUserChoice = function() {
+      if (userChoice === "R") {
+            showChoice("user-guess-result", rock);
+            userR++;
+      } else if (userChoice === "P") {
+            showChoice("user-guess-result", paper);
+            userP++;
+      } else if (userChoice === "S") {
+            showChoice("user-guess-result", scissors);
+            userS++;
+      } else {
+            showChoice("user-guess-result", "Error");
+      }
+};
 
-prompt('Type "R" for Rock, "P" for Paper, or "S" for Scissors');
+startButton.addEventListener("click", (e) => {
+      playGame();
+});
 
-showHeadings();
-showChoice("your-guess-result", rock);
-newChoice();
-displayChoice();
+
 
 
 
